19e71c1a92770b3a162a5546d1acc7f1678cfccf
AdiEnigma <adityabhowmik68@gmail.com>
2026-01-05 22:35:19 +0530

    assignment 1 2nd question

---
+++ Assignment_1_2.py
@@ -0,0 +1,22 @@
+a = [1,2,3,4,5,6]
+
+target = 5
+left = 0
+right = len(a)-1
+while left<=right:
+    
+    print(a)
+    mid = left+(right-left)//2 #middle index
+    mid_e = a[mid] #middle element
+    print(mid_e)
+    if target == mid_e:
+        print("Found")
+        break
+    elif target < mid_e:
+        right = mid - 1
+        
+        
+    else:
+        left = mid + 1
+else:
+    print("target does not exist")    
